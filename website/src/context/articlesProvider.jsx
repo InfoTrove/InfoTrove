1692ecaa4a26e27b5788bc49dc7f7c14b6bb15ff
Mohamed <mohamed.sy@compscihigh.org>
2024-03-19 15:11:23 -0400

    attempted to create context and provider

--- website/src/context/articlesProvider.jsx
+++ website/src/context/articlesProvider.jsx
@@ -4,21 +4,21 @@ import { useState } from "react";
 import handleFetch from "../utils/handleFetch"
 
 const ArticlesProvider = ( {children} ) => {
-    const [articles, setArticles] = useState([])
+    const [books, setBooks] = useState([])
     const [error, setError] = useState()
 
     useEffect(() => {
         const doFetch = async () => {
             const [data, error] = await handleFetch(
-                `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=S40TyD7zGe3HkXJZD4MiENxkBybALIxp`, {cache: "force-cache"}
+                `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=S40TyD7zGe3HkXJZD4MiENxkBybALIxp`, {cache: "force-cache"}
               );
-            if (data) setArticles(data.results)
+            if (data) setBooks(data.results)
             if (error) setError(error)
         }
     doFetch();
     }, [])
 
-    const contextValue = { articles }
+    const contextValue = { books }
 
     return (
         <ArticlesContext.Provider value={contextValue}>
--- website/src/pages/books/index.jsx
+++ website/src/pages/books/index.jsx
@@ -2,38 +2,32 @@ import { useState, useEffect } from "react";
 import handleFetch from "../../utils/handleFetch";
 import NavBar from "../../components/navbar";
 import { Button, Card } from 'react-bootstrap';
+import { useContext } from "react";
+import ArticlesContext from "../../context/articlesContext";
 
 const Books = ( ) => {
-    const [books, setBooks] = useState()
-    const [error, setError] = useState()
 
-    useEffect(() => {
-        const doFetch = async () => {
-            const [data, error] = await handleFetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=S40TyD7zGe3HkXJZD4MiENxkBybALIxp")
-            if (data) setBooks(data.results.books)
-            if (error) setError(error)
-        }
-        doFetch();
-    }, [])
+    const [error, setError] = useState()
+    const {books, setBooks} = useContext(ArticlesContext)
     console.log(books)
     return (
         <div>
             <NavBar/>
-            <ul className="flex flex-wrap gap-10 p-[50px]">
-                {
-                    books?.map((book) => (
-                        <Card style={{ width: '18rem' }} className="border-solid">
-                        <Card.Img variant="top" src={book.book_image} className="size-28" />
-                        <Card.Body>
-                          <Card.Title>{book.Title}</Card.Title>
-                          <Card.Text>
-                            {book.description}
-                          </Card.Text>
-                        </Card.Body>
-                      </Card>
-                    ))
-                }
-            </ul>
+                <ul className="flex flex-wrap gap-10 p-[50px]">
+                    {
+                        books?.map((book) => (
+                            <Card style={{ width: '18rem' }} className="border-solid">
+                            <Card.Img variant="top" src={book.book_image} className="size-28" />
+                            <Card.Body>
+                            <Card.Title>{book.Title}</Card.Title>
+                            <Card.Text>
+                                {book.description}
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        ))
+                    }
+                </ul>
         </div>
     )
 }
