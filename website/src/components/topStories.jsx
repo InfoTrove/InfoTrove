53b79bfed8e71685bfcdcf4b30846cc4149d2f97
ahmad <ahmadhamza052005@gmail.com>
2024-03-19 00:03:38 -0400

    TopStoriesAlmostComplete

--- website/src/components/topStories.jsx
+++ website/src/components/topStories.jsx
@@ -1,18 +1,70 @@
+import { useRef } from "react";
+// useRef created a Mutable Reference to a dom element . Making it intractable In React code
+// When an Element is scrollable it gains scroll properties
+// When scroll functions are invoked, they check the current scroll position
+// The containerRef contains the mutable reference to the DOM element => scrollable container
+// reference allows for manipulating its methods
+// current in ref is current rendered Element
 const TopStories = ({ stories }) => {
+  const containerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft -= 900;
+    }
+  };
+
+  const scrollRight = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += 900;
+    }
+  };
+
   return (
     <>
-    <ul>
-      {stories.map((story) => (
-        <div key={story.title}>
-        <li className=" bg-orange-500 "key={story.title}>{story.title}</li>
-        <img className = " border-red-400"key = {story.created_date}src={story.multimedia[0].url} alt={story.title} />
+      <style>
+        {`
+          .hide-scrollbar::-webkit-scrollbar {
+            display: none; /* Hide scrollbar for Chrome, Safari, Opera */
+          }
+          .hide-scrollbar {
+            -ms-overflow-style: none; /* IE and Edge */
+            scrollbar-width: none; /* Firefox */
+          }
+        `}
+      </style>
+      <div className="relative flex justify-center items-center hide-scrollbar bg-neutral-700">
+        <button
+          data-name="leftButton"
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 hover:bg-red-700"
+          onClick={scrollLeft}
+        >
+          {"<---"}
+        </button>
+        <div
+          ref={containerRef}
+          className="flex overflow-x-auto whitespace-nowrap max-w-[95%] py-3 hide-scrollbar"
+        >
+          <ul data-name="articleContainer" className="flex">
+            {stories.map((story, index) => (
+              <li className="inline-block bg-orange-500 w-96 m-8" key={index}>
+                <img
+                  className="h-96 object-cover"
+                  src={story.multimedia[0].url}
+                  alt={story.title}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-      ))}
-      <li>Stories</li>
-      <li>Stories2</li>
-      <li>Stories3</li>
-      <li>Stories4</li>
-    </ul>
+        <button
+          data-name="rightButton"
+          className="absolute right-6 top-1/2 transform -translate-y-1/2 hover:bg-red-700"
+          onClick={scrollRight}
+        >
+          {"--->"}
+        </button>
+      </div>
     </>
   );
 };
