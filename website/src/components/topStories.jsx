a50172a9952591ef8b967dcdb7331f370cb7ed24
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-03-18 20:53:22 -0400

    fixed styling for components

--- website/src/components/topStories.jsx
+++ website/src/components/topStories.jsx
@@ -1,20 +1,20 @@
 const TopStories = ({ stories }) => {
   return (
-    <div className="w-3/4 m-auto">
-      <div className="mt-20 flex overflow-x-auto space-x-4">
+    <div className="w-full m-auto">
+      <div className="mt-20 flex overflow-x-auto space-x-6">
         {stories.map((story, index) => (
           <div
             key={index}
             className="flex-none relative text-black rounded-3xl"
           >
-            <div className="w-80 h-96 relative">
+            <div className="w-96 h-auto relative">
               <img
                 src={story.multimedia[0].url}
                 alt={story.title}
-                className="absolute top-0 left-0 w-full h-full object-cover rounded-3xl"
+                className="w-full h-auto object-cover rounded-t-3xl"
               />
-              <div className="absolute bottom-0 w-full p-4 bg-white/60 dark:bg-darkBackground/60 rounded-b-3xl">
-                <p className="text-xl md:text-2xl font-semibold truncate">
+              <div className="bg-white/75 dark:bg-darkBackground/75 rounded-b-3xl p-6">
+                <p className="text-lg md:text-xl lg:text-2xl font-semibold">
                   {story.title}
                 </p>
               </div>
@@ -26,21 +26,4 @@ const TopStories = ({ stories }) => {
   );
 };
 
-// <>
-//   <ul className="flex overflow-x-auto">
-//     {stories.map((story, index) => (
-//       <div key={index}>
-//         <li className=" bg-orange-500 " key={index}>
-//           {story.title}
-//         </li>
-//         <img
-//           className=" border-red-400 size-[100% ]"
-//           key={story.created_date}
-//           src={story.multimedia[0].url}
-//           alt={story.title}
-//         />
-//       </div>
-//     ))}
-//   </ul>
-// </>
 export default TopStories;
