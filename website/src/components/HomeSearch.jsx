c5c9557fc7192d2209cc9d0d83ab3ba68c60775e
ahmad <ahmadhamza052005@gmail.com>
2024-03-21 05:34:46 -0400

    Re styled Author Component to be Mobile Compatible and made homeSearch logic a bit simpler

--- website/src/components/HomeSearch.jsx
+++ website/src/components/HomeSearch.jsx
@@ -1,114 +1,122 @@
 import { useState } from "react";
 import handleFetch from "../utils/handleFetch";
 import { useNavigate } from "react-router-dom";
+
 const SearchBar = () => {
-  // list of valid categories for books - https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=S40TyD7zGe3HkXJZD4MiENxkBybALIxp
-  // valid categories for articles - https://developer.nytimes.com/docs/articlesearch-product/1/overview
-  // movie - horror , action etc
   const apiKey = `S40TyD7zGe3HkXJZD4MiENxkBybALIxp`;
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
   const [query, setQuery] = useState("");
-  //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=movies&api-key=yourkey article/movies
 
-  const handleInputChange = (e) => {
-    setQuery(e.target.value);
+  const categoryOptions = {
+    movies: ["horror", "Action", "Comedy"],
+    articles: ["Politics", "Technology", "Health"],
+    books: ["Fiction", "Non-Fiction", "Biographies", "Science"],
   };
 
-  const handleInputFocus = () => {
-    setIsActive(true);
+  const performSearch = async (searchQuery) => {
+    const urlMap = {
+      articles: `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:("${searchQuery}")&api-key=${apiKey}`,
+      books: `https://api.nytimes.com/svc/books/v3/lists/current/${searchQuery}.json?api-key=${apiKey}`,
+      movies: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&fq=section_name:("Movies")&api-key=${apiKey}`,
+    };
+
+    const url = urlMap[selectedOption];
+    if (!url) return;
+
+    const [data, error] = await handleFetch(url);
+    if (error) {
+      console.error("Error fetching data: ", error);
+      return;
+    }
+
+    if (data && (data.response?.docs?.length > 0 || data.results)) {
+      navigate("/results", { state: { data, type: selectedOption } });
+    } else {
+      console.log("No data found for the given query.");
+    }
   };
 
-  const handleInputBlur = () => {
-    setTimeout(() => {
-      if (!query) {
-        setIsActive(false);
-      }
-    }, 200);
-  };
-
+  const handleInputChange = (e) => setQuery(e.target.value);
+  const handleInputFocus = () => setIsActive(true);
+  const handleInputBlur = () =>
+    setTimeout(() => !query && setIsActive(false), 200);
   const handleKeyDown = async (e) => {
-    if (e.key === "Enter") {
-      let url;
-      if (selectedOption === "articles") {
-        url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:("${query}")&api-key=${apiKey}`;
-      } else if (selectedOption === "books") {
-        url = `https://api.nytimes.com/svc/books/v3/lists/current/${query}.json?api-key=${apiKey}`;
-      } else if (selectedOption === "movies") {
-        url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&fq=section_name:("Movies")&api-key=${apiKey}`;
-      }
-
-      if (url) {
-        console.log(
-          `searching for ${selectedOption} category: ${query} --> ${url}`
-        );
-        const [data, error] = await handleFetch(url);
-
-        if (error) {
-          console.error("Error fetching data: ", error);
-          return;
-        }
-
-        if (
-          (data &&
-            data.response &&
-            data.response.docs &&
-            data.response.docs.length > 0) ||
-          (data && data.results)
-        ) {
-          console.log(data);
-          navigate("/results", {
-            state: {
-              data,
-              type: selectedOption, // Directly pass selectedOption as type
-            },
-          });
-        } else {
-          console.log("No data found for the given query.");
-        }
-      }
-
+    if (e.key === "Enter" && query) {
       e.preventDefault();
-    } else if (e.key === "Backspace" && query === "") {
+      await performSearch(query);
+    } else if (e.key === "Backspace" && !query) {
       setSelectedOption("");
       setIsActive(true);
     }
   };
 
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setIsActive(false);
+  };
+
+  const handleSubOptionChange = async (option) => {
+    await performSearch(option);
+    setIsActive(false);
+  };
+
   return (
-    <div>
-      <input
-        className="bg-neutral-700 w-96 rounded-full h-8 pl-4"
-        type="text"
-        value={query}
-        onChange={handleInputChange}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        onKeyDown={handleKeyDown}
-        placeholder={
-          selectedOption
-            ? `Search Category in ${selectedOption}...`
-            : "Select Category"
-        }
-      />
-      {isActive && !selectedOption && (
-        <div className="flex">
-          <button
-            className="mx-2"
-            onClick={() => setSelectedOption("articles")}
-          >
-            Articles
-          </button>
-          <button className="mx-2" onClick={() => setSelectedOption("movies")}>
-            Movies
-          </button>
-          <button className="mx-2" onClick={() => setSelectedOption("books")}>
-            Books
-          </button>
-        </div>
-      )}
-    </div>
+    <>
+      <div>
+        <input
+          className="bg-neutral-700 w-96 rounded-full h-8 pl-4 focus:outline-none text-white"
+          type="text"
+          value={query}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          onKeyDown={handleKeyDown}
+          placeholder={
+            selectedOption
+              ? `Search Category in ${selectedOption}...`
+              : "Select Category"
+          }
+        />
+        {isActive && !selectedOption && (
+          <div className="flex flex-wrap justify-center">
+            {["articles", "movies", "books"].map((option) => (
+              <button
+                key={option}
+                className="text-white bg-blue-500 hover:bg-red-700 text-sm font-bold py-2 px-4 rounded m-1"
+                onClick={() => handleOptionSelect(option)}
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+        )}
+        {selectedOption && (
+          <div className="flex flex-wrap justify-center">
+            {categoryOptions[selectedOption]?.map((option, index) => (
+              <button
+                key={index}
+                onClick={() => handleSubOptionChange(option)}
+                className="text-white bg-green-500 hover:bg-red-700 text-sm font-bold py-2 px-4 rounded m-1"
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+      <style>
+        {`
+          @media screen and (max-width: 768px) {
+            .search-bar input {
+              width: calc(100% - 2rem); /* Adjust input width on small screens */
+              margin: 0.5rem; /* Adjust margin */
+            }
+          }
+        `}
+      </style>
+    </>
   );
 };
 
