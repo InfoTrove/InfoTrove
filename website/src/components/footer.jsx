c1371d9b137717caca43fe4884884b623a0fb7b3
ahmad <ahmadhamza052005@gmail.com>
2024-03-29 07:02:05 -0400

    updated navbar Component to accept refs and made footer image scroll toTop . adjusted some footer styling as well

--- website/src/components/footer.jsx
+++ website/src/components/footer.jsx
@@ -1,15 +1,16 @@
-const Footer = () => {
+import logo from "../assets/logo.png";
+const Footer = ({ scrollToTop }) => {
   return (
     <>
       <div className="bg-[rgb(201,158,79)] h-[325px] md:p-24"></div>
       <div className="flex bg-[rgb(204,172,171)] h-[200px] ">
         <div className="flex items-center justify-center mt-10 ml-16 bg-black w-16 h-16 rounded-full text-white">
-          YamIhere
+          <img data-name="logoFooter" src={logo} alt="" onClick={scrollToTop} className="cursor-pointer" />
         </div>
 
         <div
           data-name="contact Info"
-          className="flex flex-col justify-around p-4 ml-6 mt-10 mr-8 w-[21rem] h-32 bg-[rgb(51,51,51)] border-red-600 rounded-lg text-white text-center"
+          className="flex flex-col justify-around p-1 ml-6 mt-10 mr-8 w-[21rem] h-[8.6rem] bg-[rgb(51,51,51)] border-red-600 rounded-lg text-white text-center"
         >
           <p>Contacts :</p>
           <p>Creators :</p>
@@ -18,14 +19,14 @@ const Footer = () => {
         </div>
         <div
           data-name="Info General"
-          className="flex flex-col justify-around p-4 mt-10 mx-8 w-[21rem] h-32 bg-[rgb(51,51,51)] border-red-600 rounded-lg text-white text-center"
+          className="flex flex-col justify-around p-1 mt-10 mx-8 w-[21rem] h-[8.6rem] bg-[rgb(51,51,51)] border-red-600 rounded-lg text-white text-center"
         >
           <p>Info :</p>
           <p>Data : New York Times API</p>
         </div>
         <div
           data-name="etc"
-          className="flex flex-col justify-around p-4 mt-10 mx-8 w-[21rem] h-32 bg-[rgb(51,51,51)] border-red-600 rounded-lg text-white text-center"
+          className="flex flex-col justify-around p-1 mt-10 mx-8 w-[21rem] h-[8.6rem] bg-[rgb(51,51,51)] border-red-600 rounded-lg text-white text-center"
         >
           <p>Etc :</p>
         </div>
