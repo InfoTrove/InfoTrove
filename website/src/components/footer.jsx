35ee99c0df9f84c5a9e07e0118ac128f844486a1
ahmad <ahmadhamza052005@gmail.com>
2024-03-30 00:58:28 -0400

    imagsizing

--- website/src/components/footer.jsx
+++ website/src/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = ({ scrollToTop }) => {
     <>
       {/* <div className="bg-[rgb(201,158,79)] h-[325px] md:p-24"></div> */}
       <div className="flex bg-black h-[200px] ">
-        <div className="items-center justify-center mt-10 ml-16 bg-black w-16 h-16 rounded-full text-white">
+        <div className="items-center justify-center mt-10 ml-16 bg-black w-[4.5rem] h-16 rounded-full text-white">
           <div className="scale-[3] mt-10 ml-4">
             <img
               data-name="logoFooter"
