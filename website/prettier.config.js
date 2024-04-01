76f2f91b60bf89766ef3efaafc43d88e246eddba
ahmad <ahmadhamza052005@gmail.com>
2024-04-01 14:36:33 -0400

    prettier taiwind config

---
+++ website/prettier.config.js
@@ -0,0 +1,8 @@
+// prettier.config.js
+export default {
+    plugins: ["prettier-plugin-tailwindcss"],
+  };
+  
+  // command to run on all : npx prettier --write . or npm run pretty
+  // you can make script for it in package.json . Check line 11 inside package.json
+  
